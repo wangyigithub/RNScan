2d77bd55183b305bcf4e54c0f90bcab4b089865a
wangyidong <465183544@qq.com>
2019-03-25 18:42:30 +0800

    逻辑修改

--- App.js
+++ App.js
@@ -9,13 +9,13 @@
 import React, {Component} from 'react';
 import {
     View,
-    AppRegistry,
     StyleSheet,
     Text,
     TouchableOpacity,
     Button,
     TextInput,
-    Dimensions
+    Dimensions,
+    ActivityIndicator
 } from 'react-native';
 import {createAppContainer, createStackNavigator,} from 'react-navigation';
 import QRCodeScanner from 'react-native-qrcode-scanner';
@@ -32,24 +32,33 @@ class HomeScreen extends React.Component {
         this.state = {
             text: '',
             number: 0,
+            isLoading:false
         };
     }
 
     render() {
-        const itemId=this.state.text;
-        if (this.state.number === 0) {
+        const itemId = this.state.text;
             return (
                 <View style={{flex: 1, alignItems: 'center',}}>
+                    <ActivityIndicator size="large" color="#0000ff" animating={this.state.isLoading}/>
                     <View style={{flexDirection: 'row', alignItems: 'center', margin: 10}}>
                         <Text>
                             {'请输入批次号码：'}
                         </Text>
                         <TextInput
-                            style={{height: 40, borderColor: 'gray', borderWidth: 1, marginLeft: 5, flex: 1,color: 'red'}}
+                            style={{
+                                height: 40,
+                                borderColor: 'gray',
+                                borderWidth: 1,
+                                marginLeft: 5,
+                                flex: 1,
+                                color: 'red'
+                            }}
                             onChangeText={(text) => this.setState({text})}
                             value={itemId}
                         />
                     </View>
+
                     <View style={{
                         width: screenWidth - 200,
                         flexDirection: 'row',
@@ -60,10 +69,10 @@ class HomeScreen extends React.Component {
                             title="去扫描"
                             onPress={() => {
                                 this.props.navigation.navigate('Details',{
-                                      callback: (backdata) => {
-                                       this.setState({
-                                           text:backdata,
-                                       })
+                                    callback: (backdata) => {
+                                        this.setState({
+                                            text:backdata,
+                                        })
                                     }
                                 })
                             }}
@@ -71,73 +80,43 @@ class HomeScreen extends React.Component {
                         <Button
                             title="去查询"
                             onPress={() => {
-                                if (itemId === '' || itemId === '没有批次号') return;
+                                if (itemId === '' || itemId === null || itemId === undefined) return;
+                                this.setState({
+                                    isLoading:true
+                                });
                                 this.fechData(itemId);
                             }}
                         />
                     </View>
-                </View>
-            );
-        } else {
-            return (
-                <View style={{flex: 1, alignItems: 'center',}}>
-                    <View style={{flexDirection: 'row', alignItems: 'center', margin: 10}}>
-                        <Text>
-                            {'请输入批次号码：'}
+                    {this.state.number === 0?null:<View style={{flex:1,marginTop:40,justifyContent:'center',alignItems:'center'}}>
+                        <Text style={{flexDirection: 'row',}}>
+                            批次号：<Text style={{color: 'red'}}> {itemId}</Text>
                         </Text>
-                        <TextInput
-                            style={{height: 40, borderColor: 'gray', borderWidth: 1, marginLeft: 5, flex: 1}}
-                            onChangeText={(text) => this.setState({text})}
-                            value={itemId}
-                        />
-                    </View>
-                    <Text style={{flexDirection: 'row', alignItems: 'center'}}>
-                        批次号：<Text style={{color: 'red'}}> {itemId}</Text>
-                    </Text>
-                    <View style={{
-                        width: screenWidth - 200,
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        margin: 20,
-                    }}>
-                        <Button
-                            title="去扫描"
-                            onPress={() => {
-                                this.props.navigation.navigate('Details')
-                            }}
-                        />
-                        <Button
-                            title="去查询"
-                            onPress={() => {
-                                if (itemId === '' || itemId === null||itemId===undefined) return;
-                                this.fechData(itemId);
-                            }}
-                        />
-                    </View>
-                    <Text style={{marginTop: 40, width: screenWidth,}}>杆号：<Text
-                        style={{color: 'red', fontSize: 24, fontWeight: 'bold'}}> {this.state.number}</Text></Text>
+                        <Text style={{marginTop: 10, }}> 杆号：<Text
+                            style={{color: 'red', fontSize: 24, fontWeight: 'bold'}}> {this.state.number}</Text></Text>
+                    </View>}
                 </View>
             );
-        }
     }
 
     fechData(itemId) {
-        itemId=3064001518;
+        itemId = 3064001518;
         fetch('http://220.189.234.2:17234/xiyilang/stick/search_by_makeup_code?makeup_code=' + itemId, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             }
-        }).then((response) => {//1
-            console.log(response);
-            console.log(response.data);
-            console.log(response.data.stickLogList[0]);
-            console.log(response.data.stickLogList[0].stickId);
-            this.setState({
-                number: response.data.stickLogList[0].stickId
-            })
-        }).catch((err) => {//2
+        }).then((response) => response.json())
+            .then((responseData) => {//1
+                this.setState({
+                    isLoading:false,
+                    number: responseData.data.stickLogList[0].stickId
+                })
+            }).catch((err) => {//2
             alert(err);
+            this.setState({
+                isLoading:false
+            });
         });
     }
 }
@@ -149,13 +128,14 @@ class ScannerScreen extends Component<Props> {
     };
 
     onSuccess(e) {
-        alert(e.data);
         this.goback(e.data)
     }
-    goback(itemId){
+
+    goback(itemId) {
         this.props.navigation.state.params.callback(itemId);
         this.props.navigation.goBack();
     }
+
     render() {
         return (
             <View style={styles.container}>
